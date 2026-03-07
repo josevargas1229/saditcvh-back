d2d0b1a03f7ee50ffeac49eeb183ece1ca299412
saditcvh-dev <saditcvh@gmail.com>
2026-03-06 22:07:36 -0600

    Update permission.service.js

--- src/modules/permissions/services/permission.service.js
+++ src/modules/permissions/services/permission.service.js
@@ -1,12 +1,12 @@
 const { Permission } = require("../../../database/associations");
 
 /**
- * Obtiene el catálogo completo de permisos disponibles (ver, editar, imprimir...)
+ * Obtiene el catálogo completo de permisos disponibles (ver, editar, subir...)
  */
 exports.getAllPermissions = async () => {
-    return await Permission.findAll({
-         where: { active: true, type: 'action' },
-        attributes: ['id', 'name', 'description'],
-        order: [['id', 'ASC']]
-    });
+  return await Permission.findAll({
+    where: { active: true, type: "action" },
+    attributes: ["id", "name", "description"],
+    order: [["id", "ASC"]],
+  });
 };
